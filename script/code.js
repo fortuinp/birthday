6d73a82c84cbf63d147b9d8dfb8f9946cf5082a3
fortuinp <pinda.fortuin@gmail.com>
2023-11-24 15:05:27 +0200

    2411

--- script/code.js
+++ script/code.js
@@ -1,25 +1,47 @@
-let input1=document.querySelector('[data-text]');
-let input2=document.querySelector('[data-date]');
-let output= document.getElementById('output')
+// let input1=document.querySelector('[data-text]');
+// let input2=document.querySelector('[data-date]');
+// let output= document.getElementById('output')
 
-function calcbirthday(){
-    let fullname= input1.value;
-    let todaysdate= new Date();
-   let birthday=input2.value;
-   let birthmonth= birthday.slice(5,7)
-   let birthdd= birthday.slice(8,10)
-   let currentmon=todaysdate.getMonth();
-   let currentday=todaysdate.getDay();
-   console.log(todaysdate + " mmo " + todaysdate.getMonth());
-   let days= ((birthmonth-currentmon)*30)-currentday+birthdd
-    //let daysuntil=birthday
-    console.log(birthday);
-    console.log(currentmon);
-    console.log(currentday);
-    output.textContent= fullname + " your birthday is in "+ days+ "days"
-}
+// function calcbirthday(){
+//     let fullname= input1.value;
+//     let todaysdate= new Date();
+//    let birthday=input2.value;
+//    let birthmonth= birthday.slice(5,7)
+//    let birthdd= birthday.slice(8,10)
+//    let currentmon=todaysdate.getMonth();
+//    let currentday=todaysdate.getDay();
+//    console.log(todaysdate + " mmo " + todaysdate.getMonth());
+//    let days= ((birthmonth-currentmon)*30)-currentday+birthdd
 
-document.getElementById('Bbutton').addEventListener('click',calcbirthday);
+
+//     //let daysuntil=birthday
+//     console.log(birthday);
+//     console.log(currentmon);
+//     console.log(currentday);
+//     output.textContent= fullname + " your birthday is in "+ days+ "days"
+// }
+
+// document.getElementById('Bbutton').addEventListener('click',calcbirthday);
 
 //const numbOfDays = difference / (1000 * 3600 * 24)
 // Difference between dates
+
+// let todaysdate= new Date();
+// let birthday=input2.value;
+// let birthmonth= birthday.slice(5,7)
+// let birthdd= birthday.slice(8,10)
+// let currentmon=todaysdate.getMonth();
+// let currentday=todaysdate.getDay();
+// console.log(todaysdate + " mmo " + todaysdate.getMonth());
+// let days= ((birthmonth-currentmon)*30)-currentday+birthdd
+
+// let today= new Date()
+// let currentmon=todaysdate.getMonth();
+// let birthmonth=birthday.getMonth();
+// let birthday = new Date('03/03/2002')
+// birthday.setFullYear(today.getFullYear())
+
+
+// (currentmon > birthmonth){
+//     let ourdays= birthday-today
+//     console.log(ourdays);}
